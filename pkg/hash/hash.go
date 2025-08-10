7c8cd62f3554479c160ba31753cac9dfa74ca115
atimot <tmd6031@gmail.com>
2025-08-10 12:56:43 +0900

    ハッシュパッケージ追加

---
+++ pkg/hash/hash.go
@@ -0,0 +1,17 @@
+package hash
+
+import "golang.org/x/crypto/bcrypt"
+
+func Hash(target string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(target), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashed), nil
+}
+
+func Compare(hashed, target string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(target))
+	return err
+}
